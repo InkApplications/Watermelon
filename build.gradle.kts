4bce8931e2397c2ec039673967319dbbecf992a4
Renee Vandervelde <Renee@ReneeVandervelde.com>
2020-07-02 14:57:04 -0500

    Add Kotlin and Android plugins

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+
+buildscript {
+    repositories {
+        google()
+        jcenter()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:4.0.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72")
+    }
+}
+
+subprojects {
+    repositories {
+        google()
+        jcenter()
+    }
+
+    tasks.withType(Test::class) {
+        testLogging.exceptionFormat = TestExceptionFormat.FULL
+    }
+}
