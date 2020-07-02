c8dd6ab73779ec9308e02546fea5c1ea8e7c153e
Renee Vandervelde <Renee@ReneeVandervelde.com>
2020-07-02 14:57:29 -0500

    Add Stdlib module

---
+++ stdlib-extensions/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    kotlin("multiplatform")
+}
+
+kotlin {
+    jvm()
+    js()
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-common"))
+            }
+        }
+
+        val jvmMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-jdk8"))
+            }
+        }
+
+        val jsMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-js"))
+            }
+        }
+    }
+}
