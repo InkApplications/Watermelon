5eb57348b3bed12a4ad4e2a31c1fc07627b1464c
Renee Vandervelde <Renee@ReneeVandervelde.com>
2020-09-12 10:20:16 -0500

    Add simple alpha animation extension to views

--- android/src/main/java/com/inkapplications/android/Views.kt
+++ android/src/main/java/com/inkapplications/android/Views.kt
@@ -12,6 +12,7 @@ package com.inkapplications.android
 
 import android.os.Handler
 import android.os.Looper
+import android.view.animation.AlphaAnimation
 import android.view.View
 
 private val MAIN_HANDLER = Handler(Looper.getMainLooper())
@@ -57,4 +58,42 @@ fun View.setDebouncingOnClickListener(clickListner: (View?) -> Unit) {
  */
 fun View.setVisibility(visible: Boolean, hiddenState: Int = View.GONE) {
     visibility = if (visible) View.VISIBLE else hiddenState
+}
+
+/**
+ * Animate a view visibility to Visible.
+ *
+ * Triggers a simple alpha animation, transitioning a view from invisible
+ * to visible.
+ * The view should already be invisible before calling this method.
+ *
+ * @param duration How long the animation should last.
+ * @param offset A delay before starting the animation.
+ * @see fadeOut for the opposite.
+ */
+fun View.fadeIn(duration: Int = 500, offset: Int = 0) {
+    val fade = AlphaAnimation(0f, 1f)
+    fade.duration = duration.toLong()
+    fade.startOffset = offset.toLong()
+    startAnimation(fade)
+    visibility = View.VISIBLE
+}
+
+/**
+ * Animate a view visibility to Visible.
+ *
+ * Triggers a simple alpha animation, transitioning a view from visible
+ * to invisible.
+ * The view should already be visible before calling this method.
+ *
+ * @param duration How long the animation should last.
+ * @param offset A delay before starting the animation.
+ * @see fadeIn for the opposite.
+ */
+fun View.fadeOut(duration: Int = 500, offset: Int = 0) {
+    val fade = AlphaAnimation(1f, 0f)
+    fade.duration = duration.toLong()
+    fade.startOffset = offset.toLong()
+    startAnimation(fade)
+    visibility = View.INVISIBLE
 }
