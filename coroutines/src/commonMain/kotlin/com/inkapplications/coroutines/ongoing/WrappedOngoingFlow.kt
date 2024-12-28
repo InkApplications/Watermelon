c3afb54c8f47c2a825b88440cb438b2043d2e033
Renee Vandervelde <Renee@ReneeVandervelde.com>
2024-12-28 10:20:18 -0600

    Add ongoing flow builders

--- coroutines/src/commonMain/kotlin/com/inkapplications/coroutines/ongoing/WrappedOngoingFlow.kt
+++ coroutines/src/commonMain/kotlin/com/inkapplications/coroutines/ongoing/WrappedOngoingFlow.kt
@@ -1,6 +1,9 @@
 package com.inkapplications.coroutines.ongoing
 
+import kotlinx.coroutines.awaitCancellation
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.FlowCollector
+import kotlinx.coroutines.flow.flow
 import kotlin.jvm.JvmInline
 
 /**
@@ -17,3 +20,25 @@ private value class WrappedOngoingFlow<T>(
  * Convert an existing flow to an ongoing flow.
  */
 fun <T> Flow<T>.asOngoing(): OngoingFlow<T> = WrappedOngoingFlow(this)
+
+/**
+ * Create an ongoing flow.
+ */
+inline fun <T> ongoingFlow(
+    crossinline builder: suspend FlowCollector<T>.() -> Unit,
+): OngoingFlow<T> {
+    return flow {
+        builder(this)
+        awaitCancellation()
+    }.asOngoing()
+}
+
+/**
+ * Create an ongoing flow from a set of items.
+ */
+fun <T> ongoingFlowOf(vararg items: T): OngoingFlow<T>
+{
+    return ongoingFlow {
+        items.forEach { emit(it) }
+    }
+}
--- coroutines/src/commonTest/kotlin/com/inkapplications/coroutines/ongoing/OngoingFlowTest.kt
+++ coroutines/src/commonTest/kotlin/com/inkapplications/coroutines/ongoing/OngoingFlowTest.kt
@@ -1,9 +1,15 @@
 package com.inkapplications.coroutines.ongoing
 
 import com.inkapplications.coroutines.doubles.Animal
+import kotlinx.coroutines.async
+import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.runCurrent
+import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
 import kotlin.test.assertSame
+import kotlin.test.assertTrue
 
 class OngoingFlowTest
 {
@@ -14,5 +20,39 @@ class OngoingFlowTest
         val ongoing = flow.asOngoing()
 
         assertSame(flow, ongoing.asFlow(), "Flow instance is preserved")
+    }
+
+    @Test
+    fun ongoingFlowOf()
+    {
+        runTest {
+            val ongoing = ongoingFlowOf(Animal.Bird, Animal.Cat)
+            val collected = mutableListOf<Animal>()
+
+            val job = launch { ongoing.collect { collected += it } }
+
+            runCurrent()
+            assertSame(Animal.Bird, collected[0], "First item is emitted")
+            assertSame(Animal.Cat, collected[1], "Second item is emitted")
+            assertTrue(job.isActive, "Flow does not close")
+            job.cancelAndJoin()
+        }
+    }
+
+    @Test
+    fun ongoingFlowBuilder()
+    {
+        runTest {
+            val ongoing = ongoingFlow { emit(Animal.Bird); emit(Animal.Cat) }
+            val collected = mutableListOf<Animal>()
+
+            val job = launch { ongoing.collect { collected += it } }
+
+            runCurrent()
+            assertSame(Animal.Bird, collected[0], "First item is emitted")
+            assertSame(Animal.Cat, collected[1], "Second item is emitted")
+            assertTrue(job.isActive, "Flow does not close")
+            job.cancelAndJoin()
+        }
     }
 }
